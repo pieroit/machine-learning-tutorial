aa374953cab3ab3e07f1b2ad94aa6d2c649fe131
pieroit <piero.savastano@gmail.com>
2018-06-21 20:17:40 +0200

    fix dummy code

--- boston_linear_regression_vs_dummy.py
+++ boston_linear_regression_vs_dummy.py
@@ -1,5 +1,7 @@
 
 # importiamo le funzionalita' che useremo
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 np.random.seed(1)
 from sklearn.datasets import load_boston
@@ -24,12 +26,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y)
 # addestriamo il modello sui dati di training
 model = LinearRegression()
 model.fit(X_train, y_train)
-
 # verifichiamo l'apprendimento sui dati di training
 pred_train = model.predict(X_train)
 train_score = mean_squared_error(y_train, pred_train)
 print('Train error', train_score)
-
 # testiamo il modello sui dati di test
 pred_test = model.predict(X_test)
 test_score = mean_squared_error(y_test, pred_test)
@@ -38,6 +38,23 @@ print('Test error', test_score)
 # prestazioni del modello dummy (baseline)
 dummy = DummyRegressor()
 dummy.fit(X_train, y_train)
-pred_test_dummy = dummy.predict(X_test)
-dummy_score = mean_squared_error(y_test, pred_test_dummy)
-print('Dummy test error', dummy_score)
+# dummy sul training set
+pred_train_dummy = dummy.predict(X_train)
+dummy_train_score = mean_squared_error(y_train, pred_train_dummy)
+print('Dummy train error', dummy_train_score)
+# dummy sul test set
+pred_test_dummy  = dummy.predict(X_test)
+dummy_test_score = mean_squared_error(y_test, pred_test_dummy)
+print('Dummy test error', dummy_test_score)
+
+# prepariamo dati per il grafico di confronto
+report = {
+    'train': [dummy_train_score, train_score],
+    'test' : [dummy_test_score, test_score],
+    'model': ['dummy', 'regression']
+}
+report_df = pd.DataFrame(report)
+report_df = report_df.set_index(report_df['model'])
+report_df.plot.bar(title='model error')
+
+plt.show()
