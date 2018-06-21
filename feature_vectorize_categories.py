20ed52c1944207e8cc99218d62457b755132d705
pieroit <piero.savastano@gmail.com>
2018-06-21 16:45:35 +0200

    vectorizations

---
+++ feature_vectorize_categories.py
@@ -0,0 +1,25 @@
+from sklearn.feature_extraction import DictVectorizer
+import pandas as pd
+
+# immaginiamo dei dati con una colonna categorica
+df = pd.DataFrame({
+    'altezza': [180, 200, 160, 175],
+    'peso'   : [100, 110, 80, 100],
+    'sport'  : ['rugby', 'basket', 'rugby', 'soccer']
+})
+# per utilizzare facilmente DictVectorizer passiamo per pandas
+# e convertiamo tutti i record in dizionari
+df_dictionary = df.to_dict(orient='records')
+print(df_dictionary)
+
+# vettorizziamo tutto il dataset
+vectorizer = DictVectorizer(sparse=False)
+vectorized_data = vectorizer.fit_transform(df_dictionary)
+
+print('dati vettorizzati')
+print(vectorized_data)
+
+print('riporta vettori a categorie')
+print( vectorizer.inverse_transform( vectorized_data ) )
+
+
---
+++ feature_vectorize_texts.py
@@ -0,0 +1,21 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+# immaginiamo una serie di testi (preprocessati)
+data = [
+    'ciao amico gatto',
+    'ciao amico cane',
+    'ciao ciao'
+]
+
+# vettorizziamoli
+text_vectorizer = CountVectorizer()
+vectorized_data = text_vectorizer.fit_transform(data)
+
+print('rappresentazione sparsa')
+print(vectorized_data)
+
+print('rappresentazione densa')
+print(vectorized_data.todense())
+
+print('dai vettori ritorniamo al testo')
+print( text_vectorizer.inverse_transform( vectorized_data ) )
