749922b18ddd44f1543ced97831cd3d11c77c520
pieroit <piero.savastano@gmail.com>
2018-06-22 12:55:59 +0200

    kmeans

---
+++ unsupervised_kmeans.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+
+# carica i dati
+iris_dataset = load_iris()
+print(iris_dataset['DESCR'])
+data = iris_dataset.data
+
+# lancia il clustering scegliendo il numero di cluster richiesti
+model = KMeans(n_clusters=3)
+model.fit(data)
+
+# ottieni punteggio di dispersione
+print('Inerzia', model.inertia_)
+# ottieni il cluster assegnato a ogni punto
+cluster_assignations = model.predict(data)
+print('cluster di appartenenza dei dati')
+print(cluster_assignations)
+# ottieni le coordinate dei cluster
+cluster_centers  = model.cluster_centers_
+print('posizione dei cluster')
+print(cluster_centers)
+
+# grafico
+dataframe = pd.DataFrame(data)
+dataframe.columns =['lunghezza sepalo', 'larghezza sepalo', 'lunghezza petalo', 'larghezza petalo']
+dataframe.plot.scatter(x='lunghezza sepalo', y='larghezza sepalo', c=cluster_assignations)
+dataframe.plot.scatter(x='lunghezza petalo', y='larghezza petalo', c=cluster_assignations)
+
+plt.show()
---
+++ unsupervised_kmeans_elbow.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from scikitplot.clustering import plot_elbow_curve
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+
+# carica i dati
+iris_dataset = load_iris()
+print(iris_dataset['DESCR'])
+data = iris_dataset.data
+
+# costruisci oggetto per il clustering
+model = KMeans()
+
+# lancia clustering tante volte con numero di cluster crescente
+plot_elbow_curve(model, data)
+
+plt.show()
