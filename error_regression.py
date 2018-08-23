7d418ed2a6e5c00dfbaebb9d1426039a6eb468c5
pieroit <piero.savastano@gmail.com>
2018-08-23 16:24:38 +0200

    residual plot

--- error_regression.py
+++ error_regression.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 # importiamo le funzionalita' per calcolare gli errori di un regressore
+import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
 
 # assumiano di avere un array con le risposte corrette
-y_test = [2, 1, 0, 3, 2, -1, 5]
+y_test = np.array( [2, 1, 0, 3, 2, -1, 5] )
 
 # assumiano di avere un array con le risposte predette dal modello
-y_pred = [1, 1, -1, 3, 0, 0, 4]
+y_pred = np.array( [1, 1, -1, 3, 0, 0, 4] )
 
 # calcoliamo la correttezza delle risposte
 mse = mean_squared_error(y_test, y_pred)
@@ -14,3 +16,10 @@ print('Mean square error', mse)
 
 r2 = r2_score(y_test, y_pred)
 print('R2 score', r2)
+
+residuals = y_pred - y_test
+plt.scatter(y_test, residuals)
+plt.xlabel('True')
+plt.ylabel('Error')
+
+plt.show()
